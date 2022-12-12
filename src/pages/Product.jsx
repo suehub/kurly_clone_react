743d7d0a17084d5933f0d13e04cb910dffb56c10
sue <dldustn2040@gmail.com>
2022-12-12 23:08:55 +0900

    like button isActive

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -12,6 +12,9 @@ export default function Product() {
     const [scrollY, setScrollY] = useState(0);
     const [scrollActive, setScrollActive] = useState(false);
 
+    const [like, setLike] = useState(0);    // 좋아요 개수 저장 
+    const [likeActive, setLikeActive] = useState(false); // 버튼 누른 상태 저장
+    
     const scrollFixed = () => {
         if (scrollY > 1000) {    // header height
         setScrollY(window.pageYOffset);
@@ -31,6 +34,17 @@ export default function Product() {
         window.removeEventListener('scroll', scrollFixed);
         };
     }, [scrollY, scrollActive]);
+
+    const onLikeButton = () => {
+        if(likeActive === false) {
+            setLikeActive(true);
+            setLike((prev) => prev + 1);
+        } else{
+            setLikeActive(false);
+            if(like < 1) return;
+            setLike((prev) => prev - 1);
+        }
+    }
 
     return (
         <>
@@ -378,8 +392,8 @@ export default function Product() {
                         {reviewLists.reviewArticles.map((review) => (
                         <div className="reviewList">
                             <div style={{"flex": "0 0 225px"}}>
-                                <div style={{"display":"flex","align-items":"center","margin":"-1px 0px 7px"}}>
-                                    <span className="reviewListBox">프렌즈</span>
+                                <div>
+                                    <span className="reviewListBox">일반</span>
                                     <span style={{"font-weight":"500"}}>{review.name}</span>
                                 </div>
                             </div>
@@ -396,10 +410,10 @@ export default function Product() {
                                         <div>
                                             <span style={{"color":"rgb(153,153,153)"}}>{review.date}</span>
                                         </div>
-                                        <button className="reviewButton">
+                                        <LikeHover>
                                             <span></span>
-                                            <span>도움돼요</span>
-                                        </button>
+                                            <span onClick={onLikeButton}>도움돼요 {(like != 0) && like}</span>
+                                        </LikeHover>
                                     </footer>
                                 </div>
                             </article>
@@ -493,3 +507,28 @@ const Main = styled.div`
   padding-top: 30px;
 `;
 
+const LikeHover = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 88px;
+    height: 32px;
+    padding: 0px 13px 0px 11px;
+    border: 1px solid rgb(226, 226, 226);
+    border-radius: 20px;
+    font-size: 12px;
+    line-height: 20px;
+    color: rgb(153, 153, 153);
+    span:first-of-type{
+        width: 15px;
+        height: 15px;
+        margin-right: 4px;
+        background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAxNCAxNCIgZmlsbD0ibm9uZSIKICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxwYXRoCiAgICBkPSJNNC4wNDgzNyAxMi45OTk4SDIuMjE5MzVDMS41NDU5MiAxMi45OTk4IDEgMTIuNDYyNiAxIDExLjc5OTlWNy41OTk5MkMxIDYuOTM3MTggMS41NDU5MiA2LjM5OTkzIDIuMjE5MzUgNi4zOTk5M0g0LjA0ODM3TTguMzE2MDggNS4xOTk5NVYyLjc5OTk4QzguMzE2MDggMS44MDU4OCA3LjQ5NzIgMSA2LjQ4NzA2IDFMNC4wNDgzNyA2LjM5OTkzVjEyLjk5OTlIMTAuOTI1NUMxMS41MzM1IDEzLjAwNjYgMTIuMDUzNyAxMi41NzE1IDEyLjE0NDggMTEuOTc5OUwxMi45ODYyIDYuNTc5OTNDMTMuMDM5OSA2LjIzMTg1IDEyLjkzNTUgNS44NzgxMiAxMi43MDA4IDUuNjEyNDVDMTIuNDY2IDUuMzQ2NzggMTIuMTI0NiA1LjE5NTk2IDExLjc2NjggNS4xOTk5NUg4LjMxNjA4WiIKICAgIHN0cm9rZT0iIzk5OTk5OSIgc3Ryb2tlLXdpZHRoPSIxLjEiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIKICAgIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==) center center no-repeat;
+    }
+    :hover{
+        color: rgb(95, 0, 128); 
+    }
+    :hover span:first-of-type{
+        background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAxNCAxNCIgZmlsbD0ibm9uZSIKICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxwYXRoCiAgICBkPSJNNC4wNDgzNyAxMi45OTk4SDIuMjE5MzVDMS41NDU5MiAxMi45OTk4IDEgMTIuNDYyNiAxIDExLjc5OTlWNy41OTk5MkMxIDYuOTM3MTggMS41NDU5MiA2LjM5OTkzIDIuMjE5MzUgNi4zOTk5M0g0LjA0ODM3TTguMzE2MDggNS4xOTk5NVYyLjc5OTk4QzguMzE2MDggMS44MDU4OCA3LjQ5NzIgMSA2LjQ4NzA2IDFMNC4wNDgzNyA2LjM5OTkzVjEyLjk5OTlIMTAuOTI1NUMxMS41MzM1IDEzLjAwNjYgMTIuMDUzNyAxMi41NzE1IDEyLjE0NDggMTEuOTc5OUwxMi45ODYyIDYuNTc5OTNDMTMuMDM5OSA2LjIzMTg1IDEyLjkzNTUgNS44NzgxMiAxMi43MDA4IDUuNjEyNDVDMTIuNDY2IDUuMzQ2NzggMTIuMTI0NiA1LjE5NTk2IDExLjc2NjggNS4xOTk5NUg4LjMxNjA4WiIKICAgIHN0cm9rZT0iIzVmMDA4MCIgc3Ryb2tlLXdpZHRoPSIxLjEiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIKICAgIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==) center center no-repeat;
+    }
+`;
